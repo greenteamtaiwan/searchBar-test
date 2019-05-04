2ff83626fb9479363110fc37d48dac97dab38d17
Michelle <34873832+kiecoo@users.noreply.github.com>
2019-05-04 23:22:04 +0800

    UPDATE 縮排Indent

--- src/components/SearchPage.vue
+++ src/components/SearchPage.vue
@@ -10,14 +10,14 @@
             </select>
           </div>
           <!-- input of tags -->
-			    <div id="checkboxes">
-			    	<div v-for="(stack,index) in tagWholeList" :key="index" class="form-check form-check-inline">
-				    	<input class="form-check-input" type="checkbox"  v-model="stack.checked" v-on:change="getfilteredData">
-				    	<label class="form-check-label">
-						    {{ stack.value }}
-				    	</label>
-			    	</div>
-	    		</div>
+          <div id="checkboxes">
+            <div v-for="(stack,index) in tagWholeList" :key="index" class="form-check form-check-inline">
+              <input class="form-check-input" type="checkbox"  v-model="stack.checked" v-on:change="getfilteredData">
+              <label class="form-check-label">
+                {{ stack.value }}
+              </label>
+            </div>
+          </div>
           <!-- input of word -->
           <div class="col-10">
             <input type="text" class="form-control" placeholder="Enter key word  ..." v-model="search.word" v-on:keyup="getfilteredData">
@@ -26,10 +26,10 @@
             <button type="submit" class="btn btn-primary"><i class="fa fa-search"></i></button>
           </div>
         </div>
-		</div>
+    </div>
 
     <div class="card-columns">
-			<!-- iterate data -->
+      <!-- iterate data -->
       <item-card v-for="(item, index) in filteredData" :key="index" :item="item"></item-card>
     </div>
   </div>
@@ -40,29 +40,29 @@ import ItemCard from './ItemCard';
 import data from '../data/data';
 
 export default {
-	name: 'SearchPage',
-	components: {
-		'item-card': ItemCard
-	},
-	computed: {
-		selectedTags: function() {
-			let filters = [];
-			let checkedTags = this.tagWholeList.filter(obj => obj.checked);
-			checkedTags.forEach(element => {
-				filters.push(element.value);
-			});
-			return filters;
-		}
-	},
-	data() {
-		return {
-			filteredData: [],
+  name: 'SearchPage',
+  components: {
+    'item-card': ItemCard
+  },
+  computed: {
+    selectedTags: function() {
+      let filters = [];
+      let checkedTags = this.tagWholeList.filter(obj => obj.checked);
+      checkedTags.forEach(element => {
+        filters.push(element.value);
+      });
+      return filters;
+    }
+  },
+  data() {
+    return {
+      filteredData: [],
       search: {
         word:'',
         type:'',
         tags:''
-				},
-			typesWholeList: [ "food_share","free_shop"], //所有的類別各項
+        },
+      typesWholeList: [ "food_share","free_shop"], //所有的類別各項
       tagWholeList: [
       {
         checked: false,
@@ -83,32 +83,33 @@ export default {
     ]
   };
 },
-	methods: {
-		getfilteredData: function() {
-			this.filteredData = data;
-			let filteredDataByType = [];
-			let filteredDataByTags = [];
+  methods: {
+    getfilteredData: function() {
+      this.filteredData = data;
+      let filteredDataByType = [];
+      let filteredDataByTags = [];
       let filteredDataByWord = [];
       var selectedType = this.search.type;
        // filter by type
       if (selectedType !== '') {
-				filteredDataByType = this.filteredData.filter(obj => obj.type === selectedType);
-				this.filteredData = filteredDataByType;
-			}
-			// filter by tags
-			if (this.selectedTags.length > 0) {
-				filteredDataByTags= this.filteredData.filter(obj => this.selectedTags.every(val => obj.stack.indexOf(val) >= 0));
-				this.filteredData = filteredDataByTags;
-			}
-			// filter bykeyword, for now this only affects the name attribute of each data
-			if (this.search.word !== '') {
-				filteredDataByWord = this.filteredData.filter(obj => obj.name.indexOf(this.search.word.toLowerCase()) !== -1);
-				this.filteredData = filteredDataByWord;
-			}
-		}
-	},
-	mounted() {
-		this.getfilteredData();
-	}
+        filteredDataByType = this.filteredData.filter(obj => obj.type === selectedType);
+        this.filteredData = filteredDataByType;
+      }
+      // filter by tags
+      if (this.selectedTags.length > 0) {
+        filteredDataByTags= this.filteredData.filter(obj => this.selectedTags.every(val => obj.stack.indexOf(val) >= 0));
+        this.filteredData = filteredDataByTags;
+      }
+      // filter bykeyword, for now this only affects the name attribute of each data
+      if (this.search.word !== '') {
+        filteredDataByWord = this.filteredData.filter(obj => obj.name.indexOf(this.search.word.toLowerCase()) !== -1);
+        this.filteredData = filteredDataByWord;
+      }
+    }
+  },
+  mounted() {
+    this.getfilteredData();
+  }
 };
 </script>
+
